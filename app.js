0d842ba3bc2b4b9c7435335d1505a44269ade257
mamun_131 <mamun_131@hotmail.com>
2019-09-25 00:24:48 -0400

    e

--- app.js
+++ app.js
@@ -15,26 +15,28 @@ var MaterialRowData = [];
 var Excel = require('exceljs');
 var workbook = new Excel.Workbook();
 workbook.xlsx.readFile('data1.xlsx');
+function getData() {
 console.log('ok');
-var worksheet = workbook.getWorksheet('Sheet1');
-for (i = 2; i <= worksheet.rowCount; i++) {
-    MaterialRowData.push(new MaterialData(worksheet.getRow(i).getCell(1).value,
-                                        worksheet.getRow(i).getCell(2).value, 
-                                        worksheet.getRow(i).getCell(3).value, 
-                                        worksheet.getRow(i).getCell(4).value));
+    var worksheet = workbook.getWorksheet('Sheet1');
 
+    for (i = 2; i <= worksheet.rowCount; i++) {
+        MaterialRowData.push(new MaterialData(worksheet.getRow(i).getCell(1).value,
+                                            worksheet.getRow(i).getCell(2).value, 
+                                            worksheet.getRow(i).getCell(3).value, 
+                                            worksheet.getRow(i).getCell(4).value));}
 }
 //to make array empty
 //MaterialRowData = [];
 
+getData();
 
 apps.get('/', (req,res) =>{
 
-const json = JSON.stringify(MaterialRowData);
-console.log(json); 
-res.json(json);
+    const json = JSON.stringify(MaterialRowData);
+    console.log(json); 
+    res.json(json);
 
-});
+    });
 
 //apps.listen(3000,()=> {console.log("Listinging at port 3000.....");});
 const PORT = process.env.PORT || 3000;

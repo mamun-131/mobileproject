8cbc3adcf1190f3cd216e12dd75b635f00970a02
mamun_131 <mamun_131@hotmail.com>
2019-09-25 00:45:33 -0400

    m

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var workbook = new Excel.Workbook();
 workbook.xlsx.readFile('data1.xlsx');
 
 
-apps.get('/', (req,res) =>{
+apps.get('/inventory', (req,res) =>{
     console.log('ok');
     var worksheet = workbook.getWorksheet('Sheet1');
     var MaterialRowData = [];
@@ -32,6 +32,22 @@ apps.get('/', (req,res) =>{
 
     });
 
+    apps.get('/inventory/:id', (req,res) =>{
+        // console.log('ok');
+        // var worksheet = workbook.getWorksheet('Sheet1');
+        // var MaterialRowData = [];
+        // for (i = 2; i <= worksheet.rowCount; i++) {
+        //     MaterialRowData.push(new MaterialData(worksheet.getRow(i).getCell(1).value,
+        //                                         worksheet.getRow(i).getCell(2).value, 
+        //                                         worksheet.getRow(i).getCell(3).value, 
+        //                                         worksheet.getRow(i).getCell(4).value));}
+        // const json = JSON.stringify(MaterialRowData);
+        // console.log(json); 
+        // res.json(json);
+        res.send('user: ' + req.params.id); 
+        });
+
+//const result = inventory.find( ({ name }) => name === 'cherries' );
 //apps.listen(3000,()=> {console.log("Listinging at port 3000.....");});
 const PORT = process.env.PORT || 3000;
 apps.listen(PORT, () => {
